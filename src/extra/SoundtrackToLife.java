20412ccb987c4674880b7e64e36edd7eca6f27ea
AnkitBisain <league@wts-im-6.attlocal.net>
2018-02-14 19:32:27 -0800

    soundtracktolife highlowgame

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -11,14 +11,33 @@ public class SoundtrackToLife {
 	public static void main(String[] args) {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+		int userMood = JOptionPane.showOptionDialog(null, "What is your MOOD?", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Sad", "Happy", "Angry","Jealous","Frustrated","Heartbroken","In Love" }, null);
 		
 		// 2. Their answer is stored in the userMood variable. Print it out.
-
+System.out.println(userMood);
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
-
+		if(userMood==0) {
+			playVideo("https://www.youtube.com/watch?v=J_ub7Etch2U");
+		}
+		else if(userMood==1) {
+			playVideo("https://www.youtube.com/watch?v=IdneKLhsWOQ");
+		}
+		else if(userMood==2) {
+			playVideo("http://bit.ly/video-for-angry");
+		}
+		else if(userMood==3) {
+			playVideo("https://www.youtube.com/watch?v=Dmxn8OKarag");
+		}
+		else if(userMood==4) {
+			playVideo("https://www.youtube.com/watch?v=-Rl6KH7MKcc");
+		}
+		else if(userMood==5) {
+			playVideo("https://www.youtube.com/watch?v=SR6iYWJxHqs");
+		}
+		else if(userMood==6) {
+			playVideo("https://www.youtube.com/watch?v=LjhCEhWiKXk&list=RDQMCuWWsgWD_mw");
+		}
 		
 		// 4. Play different songs for other moods.
 
